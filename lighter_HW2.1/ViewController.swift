df492a5556841aab2ebb13899dc05fe71284b62b
Mila <mila.liutok@gmail.com>
2021-01-27 21:38:46 +0200

    Initial Commit

---
+++ lighter_HW2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lighter_HW2.1
+//
+//  Created by Lyudmila Tokar on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
