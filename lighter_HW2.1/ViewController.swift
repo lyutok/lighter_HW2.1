afb7c8a8d15c29f39323d00868bfc0af06aeabe1
Mila <mila.liutok@gmail.com>
2021-01-27 22:58:02 +0200

    task 2 lesson 1

--- lighter_HW2.1/ViewController.swift
+++ lighter_HW2.1/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var buttonPressText: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        redLightView.layer.cornerRadius = 45
+        yellowLightView.layer.cornerRadius = 45
+        greenLightView.layer.cornerRadius = 45
+        
+        buttonPressText.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func buttonPressed() {
+        
+    }
 }
 
